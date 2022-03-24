bd9282c8141a49509f3813d6090df9db7063a3a6
calvinalexander <m0050525@bangkit.academy>
2022-03-24 15:45:24 +0700

    push modules

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(
    description='A useful module',
    author='unk',
    author_email='unk@mail.com',
-   packages=['eda', 'models', 'utils'],  #same as name
-   install_requires=['multimethod ', 'tangled_up_in_unicode'],
+   packages=['eda', 'models', 'utils'],
 )
